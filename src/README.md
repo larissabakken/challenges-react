dbc6543ebf74619c1c8c307374547a59c094510b
Larissa Ramos <devlarissasr@gmail.com>
2023-05-25 10:51:04 -0300

    :construction:

---
+++ src/README.md
@@ -0,0 +1,20 @@
+ ### Challenges to learn React
+    - [x] [ReactJS](https://reactjs.org/)
+
+Install the below tools/packages to run the project
+
+#### Tools/packages
+- [x] [NodeJS](https://nodejs.org/en/) >= 16.17.0
+- [x] [NPM](https://www.npmjs.com/) >= 9.0.0
+
+#### How to run the project
+```bash
+# Clone this repository
+$ git clone
+
+# Run the command
+$ npm install
+
+# Run the command
+$ npm dev
+```
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './global.css'
+
+import Home from './pages/home'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
